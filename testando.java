6f696249dbbf616f1eca57348fbda35e64c3b12e
Queli Gramacho <112115486+Quelii@users.noreply.github.com>
2022-11-18 13:47:34 -0300

    Add files via upload

---
+++ testando.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.atomic.*;
+import java.util.Scanner; 
+import java.util.Locale;
+
+public class testando{
+
+/**
+ * @param args
+ */
+public static void main(string[]args){
+Locale.setDefault(Locale.US);
+
+
+
+}
+
+
+
+
+
+}
